fc296f430bd453f9ffe67b2f8c8d33b650448d13
Slava Shpitalny <slavik57@gmail.com>
2016-03-09 20:52:43 +0200

    fix a little

--- typings/es6-extension.d.ts
+++ typings/es6-extension.d.ts
@@ -1,5 +1,5 @@
 declare module 'es6-promise/dist/es6-promise' {
     export interface Promise <R> {
-      finally?<U>(onFinally?: () => U | Thenable<U>): Thenable<U>;
+      finally?<U>(onFinally?: () => U | Promise<U>): Promise<U>;
     }
 }
